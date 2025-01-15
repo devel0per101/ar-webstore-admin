5ed28ebae8c35b05debae6b9250c7ba399a53e38
devel0per101 <omkarvbhosale28@gmail.com>
2025-01-16 02:45:17 +0530

    remove collection in product

--- components/custom ui/MultiSelect.tsx
+++ components/custom ui/MultiSelect.tsx
@@ -13,6 +13,8 @@ import {
     CommandSeparator,
     CommandShortcut,
 } from "@/components/ui/command"
+import { Badge } from "../ui/badge"
+import { X } from "lucide-react"
 
 interface MultiSelectProps {
     placeholder: string
@@ -26,29 +28,44 @@ const MultiSelect: React.FC<MultiSelectProps> = ({ placeholder, collections, val
     const [inputValue, setInputValue] = useState("");
     const [open, setOpen] = useState(false);
 
-    console.log(value)
+    let selected: CollectionType[]
+
+    if (value.length === 0) {
+        selected = []
+    } else {
+        selected = value.map((id) => collections.find((collection) => collection._id === id)) as CollectionType[]
+    }
 
     return (
         <Command className="overflow-visible bg-white">
-            <CommandInput 
-                placeholder={placeholder} 
-                value={inputValue} 
-                onValueChange={setInputValue} 
-                onBlur={() => setOpen(false)}
-                onFocus={() => setOpen(true)} 
-            />
+            <div className="flex gap-1 flex-wrap border rounded-md">
+                {selected.map((collection) => (
+                    <Badge key={collection._id}>{collection.title}
+                        <button className="ml-1 hover:text-red-1" onClick={() => onRemove(collection._id)}>
+                            <X className="h-3 w-3" />
+                        </button>
+                    </Badge>
+                ))}
+                <CommandInput
+                    placeholder={placeholder}
+                    value={inputValue}
+                    onValueChange={setInputValue}
+                    onBlur={() => setOpen(false)}
+                    onFocus={() => setOpen(true)}
+                />
+            </div>
 
             <div className="relative mt-2">
                 {open && (
                     <CommandGroup className="absolute w-full z-10 top-0 overflow-auto border rounded-md shadow-md">
                         {collections.map((collection) => (
                             <CommandItem
-                            key={collection._id}
-                            onMouseDown={(e) => e.preventDefault()}
-                            onSelect={() => {onChange(collection._id)}}
-                        >
-                            {collection.title}
-                        </CommandItem>
+                                key={collection._id}
+                                onMouseDown={(e) => e.preventDefault()}
+                                onSelect={() => { onChange(collection._id) }}
+                            >
+                                {collection.title}
+                            </CommandItem>
                         ))}
                     </CommandGroup>
                 )
